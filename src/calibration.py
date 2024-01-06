058b7df2cff07aaa5ed24a6a2b84b91238ba9eaa
d4n13lx13 <danxie2001@gmail.com>
2024-01-06 14:38:35 -0500

    initial calibration tool program

--- src/calibration.py
+++ src/calibration.py
@@ -7,20 +7,30 @@ class Calibration:
 
     def __init__(self, bag, verify_calibration=True):
         self.bag = bag
-        self.bridge = cv_bridge()
+        self.bridge = cv_bridge.CvBridge()
         self.calib_images = []
         self.img_points = []
         self.objpoints = []
         self.test_image = None
+        self.img_width = 0
+        self.img_height = 0
         self.verify_calibration = verify_calibration
 
-    def calibrate_images(self, pattern_size=(6, 6), square_size=0.108):
+    def calibrate_images(self, pattern_size=(6,10), square_size=0.020):
         
         self.get_pattern_corners(square_size, pattern_size=pattern_size)
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.img_points, self.calib_images[0].shape[::-1], None, None)
         
-        if self.verify_calibration:
-            self.validate_calibration(mtx, dist, rvecs, tvecs)
+        if ret:
+            print("Calibration successful.")
+            if self.verify_calibration:
+                self.validate_calibration(mtx, dist, rvecs, tvecs)
+
+            # camera matrix, distortion coefficients, rotation and translation vectors
+            return ret, mtx, dist, rvecs, tvecs, self.img_height, self.img_width
+        else:
+            print("Calibration failed.")
+            return ret, None, None, None, None, 0, 0
 
     def validate_calibration(self, mtx, dist, rvecs, tvecs):
         h, w = self.test_image.shape[:2]
@@ -45,6 +55,8 @@ class Calibration:
     def get_pattern_corners(self, square_size, topic='/camera/image_raw', sample_size=10, pattern_size=(6, 6)):
         # Iterate over the messages in the bag file
         
+        print("Getting pattern corners...")
+
         num_frames = self.get_num_frames()
         init_image = False
         i=0
@@ -61,23 +73,30 @@ class Calibration:
         for topic, msg, t in self.bag.read_messages(topics=[topic]):
             # Convert the ROS image message to OpenCV format
             cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-
+            cv2.imshow('img', cv_image)
+            cv2.waitKey(1)
             if not init_image:
-
+                self.img_width, self.img_height = cv_image.shape[:2]
                 # Check if checkerboard pattern is detected
                 gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+                print(t)
                 found, corners = cv2.findChessboardCorners(gray_image, pattern_size)
                 if found:
                     # Do something with the image
                     self.calib_images.append(cv_image)
-                    self.img_points.append(corners)
+                    corners2 = cv2.cornerSubPix(gray_image,corners, (11,11), (-1,-1), criteria)
+                    self.img_points.append(corners2)
                     self.objpoints.append(objp)
+                
+                    cv2.drawChessboardCorners(cv_image, pattern_size, corners2, found)
+                    # cv2.imshow('img', cv_image)
+                    # cv2.waitKey(500)
                     init_image = True
                     floor = i
                     j+=1
                     frames_between = int(num_frames / (sample_size+1))
             else:
-                if j == sample_size:
+                if j == sample_size - 1:
                     self.test_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
                 elif i == floor + frames_between*j:
                     # Check if checkerboard pattern is detected
@@ -86,8 +105,14 @@ class Calibration:
                     if found:
                         # Do something with the image
                         self.calib_images.append(cv_image)
-                        self.img_points.append(corners)
+                        corners2 = cv2.cornerSubPix(gray_image,corners, (11,11), (-1,-1), criteria)
+                        self.img_points.append(corners2)
                         self.objpoints.append(objp)
+                    
+                        cv2.drawChessboardCorners(cv_image, pattern_size, corners2, found)
+                        # cv2.imshow('img', cv_image)
+                        # cv2.waitKey(500)
+
                 j+=1
             
             i+=1
@@ -102,3 +127,6 @@ class Calibration:
             num_frames += 1
 
         return num_frames
+    
+    def close(self):
+        cv2.destroyAllWindows()
--- src/intrinsic_tool.py
+++ src/intrinsic_tool.py
@@ -1,25 +1,63 @@
 import argparse
 import rosbag
 from calibration import Calibration
+import yaml
+import os
+import shutil
 
 
 def get_bag(bag_path):
     bag = rosbag.Bag(bag_path)
     return bag
 
+def write_cals(mtx, dist, img_height, img_width, path):
+    with open(path, 'r') as f:
+        cals = yaml.load(f, Loader=yaml.FullLoader)
+
+                        #fu     fv      u0      v0
+    cals["intrinsics"] = [mtx[0][0], mtx[1][1], mtx[0][2], mtx[1][2]]
+    cals["distortion_coeffs"] = [dist[0], dist[1], dist[0][2], dist[0][3], dist[0][4]]
+    cals["resolution"] = [img_width, img_height]
+
+    with open(path, "w") as f:
+        yaml.dump(cals, f)
+
+def create_new_cal(cal_name):
+    template_path = ".../template/chain_template.yaml"
+    output_path = f".../output_cals/{cal_name}.yaml"
+    shutil.copyfile(template_path, output_path)
+    return output_path
+
 def main():
     # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('bag', help='Path to ROS bag file')
 
-    args = parser.parse_args()
+    try:
 
-    # Open the ROS bag file
-    bag = rosbag.Bag(args.bag)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-b', '--bag', help='Path to ROS bag file')
+        parser.add_argument('-n', '--name', help='Name of calibration file (default: chain.yaml)', default='chain.yaml')
 
-    calibration = Calibration(bag)
-    calibration.calibrate_images()
+        args = parser.parse_args()
 
-    
+        # Open the ROS bag file
+        bag = rosbag.Bag(args.bag)
+        
+        print("Opened bag file")
+
+        calibration = Calibration(bag)
+        ret, mtx, dist, rvecs, tvecs, img_height, img_width = calibration.calibrate_images()
+
+        if ret:
+            if not os.path.exists(".../output_cals"):
+                os.mkdir("../output_cals")
+            
+            output_cal = create_new_cal(args.name)
+
+            write_cals(mtx, dist, img_height, img_width, output_cal)
+    except KeyboardInterrupt:
+        print("Interrupted by user, shutting down")
+        exit(0)
+
+
 if __name__ == "__main__":
     main()
